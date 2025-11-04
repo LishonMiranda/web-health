25ee51134f90abfece2e4e82b89fd95dfe6f515c
LishonMiranda <lishonmiranda@gmail.com>
2025-11-04 22:31:31 +0530

    adding a missing file

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// TODO: Replace with your actual Firebase project configuration
+export const firebaseConfig = {
+    apiKey: "AIzaSyC9HRV4X0VuyJT5JDSdWuJXkrcT_h8vJBQ",
+    authDomain: "web-health-dc529.firebaseapp.com",
+    projectId: "web-health-dc529",
+    storageBucket: "web-health-dc529.firebasestorage.app",
+    messagingSenderId: "147046696848",
+    appId: "1:147046696848:web:916d51d6aa0943f44e9515",
+    measurementId: "G-SD0VC4HPF3"
+};
