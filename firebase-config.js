7d6ff291f32a6bd3f98d0907009494ac4719ea1d
LishonMiranda <lishonmiranda@gmail.com>
2025-11-05 20:58:33 +0530

    Update Firebase configuration details

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
 // TODO: Replace with your actual Firebase project configuration
 export const firebaseConfig = {
-    apiKey: "AIzaSyC9HRV4X0VuyJT5JDSdWuJXkrcT_h8vJBQ",
-    authDomain: "web-health-dc529.firebaseapp.com",
-    projectId: "web-health-dc529",
-    storageBucket: "web-health-dc529.firebasestorage.app",
-    messagingSenderId: "147046696848",
-    appId: "1:147046696848:web:916d51d6aa0943f44e9515",
-    measurementId: "G-SD0VC4HPF3"
+    apiKey: "AIzaSyAfbv1cwJwYunyY1igtDzVtmEt2N-1Nbe4",
+    authDomain: "web-health-a3013.firebaseapp.com",
+    projectId: "web-health-a3013",
+    storageBucket: "web-health-a3013.firebasestorage.app",
+    messagingSenderId: "918077546175",
+    appId: "1:918077546175:web:a0a4e5c18a6b0307dbb00a",
+    measurementId: "G-ZZ21ETQ10K"
 };
