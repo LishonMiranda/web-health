1fa0382ff76c77453d4911202b62020b50729be3
LishonMiranda <lishonmiranda@gmail.com>
2025-11-05 21:24:53 +0530

    Fix Firebase config and script paths in index.html
    Corrected the storageBucket URL in firebase-config.js to use the proper appspot.com domain. Removed the <base> tag and updated script src paths in index.html to ensure correct module loading.

--- firebase-config.js
+++ firebase-config.js
@@ -3,7 +3,7 @@ export const firebaseConfig = {
   apiKey: "AIzaSyAfbv1cwJwYunyY1igtDzVtmEt2N-1Nbe4",
   authDomain: "web-health-a3013.firebaseapp.com",
   projectId: "web-health-a3013",
-  storageBucket: "web-health-a3013.firebasestorage.app",
+  storageBucket: "web-health-a3013.appspot.com",  
   messagingSenderId: "918077546175",
   appId: "1:918077546175:web:a0a4e5c18a6b0307dbb00a",
   measurementId: "G-ZZ21ETQ10K"
